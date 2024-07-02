268268e51c3f4e1e1546a0d38566e78252cb65ac
theeugenechong <eugenechong20011@gmail.com>
2024-07-02 15:14:24 +0800

    fix hyperlinks in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,10 @@ const Footer = () => {
             <Layout className="py-8 flex items-center justify-between lg:flex-col lg:py-6">
                 <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
                 <div className="flex items-center lg:py-2">
-                    Built with <span className="text-primary dark:text-primaryDark text-2xl px-1">&#9825;</span> by&nbsp;<Link href="/" className="underline underline-offset-2">Eugene</Link>
+                    Built with <span className="text-primary dark:text-primaryDark text-2xl px-1">&#9825;</span> by&nbsp;<Link href="https://www.linkedin.com/in/eugene-chongg/" className="underline underline-offset-2">Eugene</Link>
                 </div>
                 
-                <Link href="/" className="underline underline-offset-2">Say hello!</Link>
+                <Link href="mailto:eugene.chong@u.nus.edu" className="underline underline-offset-2">Say hello!</Link>
             </Layout>
         </footer>
     );
